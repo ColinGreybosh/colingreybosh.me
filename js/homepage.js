f794ab1d89ec4576560522ee3cddcf52848e2447
ErgsAndTerst <coling1231999@hotmail.com>
2016-11-08 17:08:06 -0500

    Added JS functionality to burgerList

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,12 @@
-// function burgerClick() {
+function burgerClick() {
+    var burger = document.getElementById('burgerList');
+    if (!isVisible('burgerList')) {
+        burger.style.display = 'block';
+    } else {
+        burger.style.display = 'none';
+    }
+}
 
-// }
+function isVisible(idName) {
+    return document.getElementById(idName).style.display == 'block';
+}
