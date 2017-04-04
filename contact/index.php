3bdc8baf16055c5c3c580310604e658c7959b7a8
ColinGreybosh <cgreybosh99@gmail.com>
2017-04-03 20:04:18 -0400

    Formatted htmlBody echo

--- contact/index.php
+++ contact/index.php
@@ -54,11 +54,9 @@
         echo '<p><strong>Captcha: </strong>'.$captcha.'</p>';
 
         $htmlBody = 
-        '
-            <p><b>From: </b>'.$name.' \<'.$email.'\></p>
+            '<p><b>From: </b>'.$name.' &lt;'.$email.'&lt;</p>
             <p><b>Message:</b></p>
-            <p>'.$message.'</p>
-        ';
+            <p>'.$message.'</p>';
         echo '<p><strong>HtmlBody: </strong></p>'.$htmlBody;
 
         if ($_POST['g-recaptcha-response']) 
